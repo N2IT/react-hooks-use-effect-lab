e722a25595333406e13b798c79999606a0f126a4
Tony Eder <tonyeder11@gmail.com>
2023-12-15 10:03:24 -0800

    countdown to 10, reset at zero good to go

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1)}, 1000);
+
+      
+      return function cleanup() {
+        if(timeRemaining === 0){
+        setTimeRemaining(10)
+      }
+
+    }})
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
